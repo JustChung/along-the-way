3b5748e2cf7de374d7ce9f5079f7a11a07eff594
justchung <justinjwchung@gmail.com>
2024-11-21 18:36:14 -0800

    modifies RouteCard

--- ui/src/components/RouteCard/RouteCard.tsx
+++ ui/src/components/RouteCard/RouteCard.tsx
@@ -3,7 +3,7 @@
 import { Input, Card, CardBody, Typography, CardFooter, Button } from "@material-tailwind/react";
 import React from 'react';
 import { Restaurant } from '../../types';
-import { MapPinIcon } from "@heroicons/react/24/solid";
+import { MapPinIcon, MapIcon } from "@heroicons/react/24/outline";
 
 interface RouteCardProps {
     source?: string;
@@ -17,14 +17,14 @@ const RouteCard: React.FC<RouteCardProps> = ({ source, destination, stops, onSel
     <div className="space-y-4">
       <Card className="mt-6 w-96" onClick={onSelect}>
         <CardBody>
-          <Typography variant="h5" color="blue-gray" className="mb-2">
-            <span className="flex gap-2"><img src="/route-icon.png" className="w-8 p-1"></img>Route</span>
+          <Typography variant="h5" color="blue-gray" className="mb-2 gap-2 flex items-center">
+            <MapIcon className="w-8 p-1"/><span className="text-center">Route</span>
           </Typography>
           <Typography color="blue-gray" className="mb-2">
-          <span className="flex gap-2 border-2 rounded-md"><MapPinIcon className="w-5" color="#00AB41"/>{source ? source : "Source"}</span>
+          <span className="flex gap-2 border-2 rounded-md p-2"><MapPinIcon className="w-5" color="#00AB41"/>{source ? source : "Source"}</span>
           </Typography>
           <Typography color="blue-gray" className="mb-2">
-           <span className="flex gap-2 border-2 rounded-md"><MapPinIcon className="w-5" color="#FF474C"/>{destination ? destination : "Destination"}</span>
+           <span className="flex gap-2 border-2 rounded-md p-2"><MapPinIcon className="w-5" color="#FF474C"/>{destination ? destination : "Destination"}</span>
           </Typography>
           {/* <Input label="Input With Icon" value={source} icon={<i className="fas fa-heart" />} />
           <Input label="Input With Icon" value={destination} icon={<i className="fas fa-heart" />} /> */}
